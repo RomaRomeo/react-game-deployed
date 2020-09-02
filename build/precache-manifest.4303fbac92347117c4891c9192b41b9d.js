3ca0b7939d8d9a6a0766b2776ae62fc27aabfce3
RomaRomeo <r.pidluzhnyi@ukr.net>
2020-09-02 14:14:37 +0300

    deploy app

---
+++ build/precache-manifest.4303fbac92347117c4891c9192b41b9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c66ac16378db3a907e4e4d6aab8ce6d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4083939c62f1f3c1e11",
+    "url": "/static/css/main.6542bd51.chunk.css"
+  },
+  {
+    "revision": "56aa5efe18bd26fbb86c",
+    "url": "/static/js/2.ecc28fa7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ecc28fa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4083939c62f1f3c1e11",
+    "url": "/static/js/main.765bb0c1.chunk.js"
+  },
+  {
+    "revision": "a1a6e13893ad5c36c36b",
+    "url": "/static/js/runtime-main.f603760a.js"
+  }
+]);
